7f59b3632e94c28ae2d11a088a3165c1f9aad21f
ALIJIH <ALIJIHERNANDEZ@GMAIL.COM>
2021-04-05 01:13:23 -0300

    antes de pull

--- VS_Project/FullViajes/FullViajes/App_Start/WebApiConfig.cs
+++ VS_Project/FullViajes/FullViajes/App_Start/WebApiConfig.cs
@@ -12,6 +12,8 @@ namespace FullViajes
         {
             //config.MapHttpAttributeRoutes();
             config.EnableCors(new EnableCorsAttribute("http://localhost:4200", headers: "*", methods: "*"));
+            EnableCorsAttribute cors = new EnableCorsAttribute("*", "*", "GET,POST,PUT,DELETE,OPTIONS");
+            config.EnableCors(cors);
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
