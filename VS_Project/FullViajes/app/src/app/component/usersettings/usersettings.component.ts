e922ecb8ea38e3eaa8091748f2a128e9d6073c7b
Matías Herrera <mjrherrera@gmail.com>
2021-05-03 16:09:39 -0300

    volviendo a commit de Ali

--- VS_Project/FullViajes/app/src/app/component/usersettings/usersettings.component.ts
+++ VS_Project/FullViajes/app/src/app/component/usersettings/usersettings.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-usersettings',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./usersettings.component.css']
 })
 export class UsersettingsComponent implements OnInit {
+  user_id="";
+  user:any;
 
-  constructor() { }
+  constructor(public service: UsuarioService, private router: Router) { }
 
   ngOnInit(): void {
+    this.user_id = localStorage.getItem('user_id')!;
+    this.service.acceder(this.user_id).subscribe(
+      (usuario: any) => {
+        this.user=usuario;
+      }
+    );
   }
 
 }
+
