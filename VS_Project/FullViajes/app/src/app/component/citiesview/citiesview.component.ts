e7cb90a3342196edce5ab3984d600d08efd960f7
RomanEtulain <romanetulain27@gmail.com>
2021-05-07 09:50:53 -0300

    Añadiendo clima especifico a cityview

--- VS_Project/FullViajes/app/src/app/component/citiesview/citiesview.component.ts
+++ VS_Project/FullViajes/app/src/app/component/citiesview/citiesview.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CiudadesService } from 'src/app/services/ciudades.service';
+import { ClimaService } from 'src/app/services/clima.service';
 
 @Component({
   selector: 'app-citiesview',
@@ -10,16 +11,34 @@ import { CiudadesService } from 'src/app/services/ciudades.service';
 export class CitiesviewComponent implements OnInit {
   city_id="";
   city:any;
+  weather:any;
 
-  constructor(public service: CiudadesService, private router: Router, private _route:ActivatedRoute) { }
+  constructor(public service: CiudadesService, private router: Router, private _route:ActivatedRoute, private weatherService: ClimaService) { }
 
   ngOnInit(): void {
     this.city_id = this._route.snapshot.paramMap.get('id')!;
     this.service.acceder(this.city_id).subscribe(
       (ciudad: any) => {
-      this.city=ciudad
+      this.city=ciudad;
+      this.getWeather(this.city.nombre);
       }
     );
+    
+  }
+
+  getWeather(cityName: string) {
+    this.weatherService
+      .getClima(cityName)
+      .subscribe(
+        res => {
+          console.log(res);
+          this.weather = res;
+        },
+        err => {
+          console.log(err);
+        }
+      );
+      
   }
 
 }
